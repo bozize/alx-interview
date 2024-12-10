60f89249e5f2bfc81bffa262e0c808c741284c04
boziz <abdiazizibrahim56@gmail.com>
2024-12-10 11:56:17 +0300

    fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
 """
-Enas modylo: oρισmaei mia sinartisi pou ypologizei
-to perimetro enos nisiou se ena dosmeno pinaka
+A module: defines a function that calculates
+the perimeter of an island on a given grid
 """
 
-def perimetros_nisiou(pinakas):
+
+def island_perimeter(grid):
     """
-    Epistrefei to perimetro enos nisimou ston dosmeno pinaka
-
-    Parametroi:
-        pinakas (list of list of int): 2D pinakas me 0 (nero) kai 1 (xora).
-
-    Epistrofi:
-        int: To perimetro tou nisiou sto pinaka.
+    Returns the perimeter of any notable island in the given grid
     """
-    mikos = len(pinakas)
-    pi = len(pinakas[0])  # 'breath' translated to Greek 'pi' for "width"
-    perimetro = 0
-    for x in range(mikos):
-        for y in range(pi):
-            if pinakas[x][y] == 1:
-                perimetro += 4
-                if (y < pi - 1 and pinakas[x][y + 1] == 1):
-                    perimetro -= 2
-                if (x < mikos - 1 and pinakas[x + 1][y] == 1):
-                    perimetro -= 2
-    return perimetro
-
+    length = len(grid)
+    breath = len(grid[0])
+    perimeter = 0
+    for x in range(length):
+        for y in range(breath):
+            if grid[x][y] == 1:
+                perimeter += 4
+                if (y < breath - 1 and grid[x][y + 1] == 1):
+                    perimeter -= 2
+                if (x < length - 1 and grid[x + 1][y] == 1):
+                    perimeter -= 2
+    return perimeter
