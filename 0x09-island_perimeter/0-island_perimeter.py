06277ffaf8d0663ecfa2a525ea26ac6512200d33
boziz <abdiazizibrahim56@gmail.com>
2024-12-10 11:46:50 +0300

    fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,54 +1,60 @@
 #!/usr/bin/python3
-"""
-Module to calculate the perimeter of an island described in a grid.
+"""Perimeter of an Island - ALX Technical Interview"""
 
-Each cell of the grid represents either land (1) or water (0).
-The function calculates the perimeter of the island formed by land cells.
+def elegxos_aksiotitas(x):
+    """Check if the value is 0 or 1.
 
-The perimeter is defined as the number of edges of land cells that are adjacent to water cells or the boundary of the grid.
-
-Parameters:
-    grid (list of list of int): A 2D grid where 1 represents land and 0 represents water.
-
-Returns:
-    int: The perimeter of the island.
-"""
-def perimetros_nisou(diktio):
-    """
-    Calculate the perimeter of an island described in a grid.
-
-    Parameters:
-        diktio (list of list of int): A grid where 1 represents land and 0 represents water.
+    Args:
+        x (int): The value to check.
 
     Returns:
-        int: The perimeter of the island.
+        int: Returns 1 if the value is 0, otherwise returns 0.
     """
-    # Get the number of rows and columns in the grid
-    grammes = len(diktio)
-    stiloi = len(diktio[0])
+    if x == 0:
+        return 1
+    return 0
+
+
+def perimetros_nisiou(pinakas):
+    """Calculate the perimeter of an island in a grid.
+
+    Args:
+        pinakas (list of list of int): 2D grid representing the island where
+                                       1's represent land and 0's represent water.
+
+    Returns:
+        int: The total perimeter of the island.
+    """
+
+    arithmos_grammes = len(pinakas)
+    arithmos_stiles = len(pinakas[0])
+    assert 1 <= arithmos_grammes <= 100 and 1 <= arithmos_stiles <= 100, "The dimensions must be between 1 and 100"
+
     perimetros = 0
+    for i in range(arithmos_grammes):
+        for j in range(arithmos_stiles):
+            assert pinakas[i][j] == 0 or pinakas[i][j] == 1, "Grid values must be either 0 or 1"
+            
+            if pinakas[i][j] == 1:
+                if i - 1 < 0:
+                    perimetros += 1
+                else:
+                    perimetros += elegxos_aksiotitas(pinakas[i-1][j])
 
-    # Iterate through each cell in the grid
-    for grammh in range(grammes):
-        for stilos in range(stiloi):
-            # If the current cell is land (1)
-            if diktio[grammh][stilos] == 1:
-                # Check the four neighbors (top, bottom, left, right)
+                if j - 1 < 0:
+                    perimetros += 1
+                else:
+                    perimetros += elegxos_aksiotitas(pinakas[i][j-1])
 
-                # Check the top (if we're on the first row or the cell above is water)
-                if grammh == 0 or diktio[grammh - 1][stilos] == 0:
+                try:
+                    perimetros += elegxos_aksiotitas(pinakas[i+1][j])
+                except IndexError:
                     perimetros += 1
 
-                # Check the bottom (if we're on the last row or the cell below is water)
-                if grammh == grammes - 1 or diktio[grammh + 1][stilos] == 0:
-                    perimetros += 1
-
-                # Check the left (if we're on the first column or the cell to the left is water)
-                if stilos == 0 or diktio[grammh][stilos - 1] == 0:
-                    perimetros += 1
-
-                # Check the right (if we're on the last column or the cell to the right is water)
-                if stilos == stiloi - 1 or diktio[grammh][stilos + 1] == 0:
+                try:
+                    perimetros += elegxos_aksiotitas(pinakas[i][j+1])
+                except IndexError:
                     perimetros += 1
 
     return perimetros
+
