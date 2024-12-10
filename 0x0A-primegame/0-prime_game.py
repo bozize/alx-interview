162f01ddaac70d150f0a096213ccdb55f3b5ac89
boziz <abdiazizibrahim56@gmail.com>
2024-12-10 12:07:29 +0300

    fixed

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,70 @@
+#!/usr/bin/python3
+"""
+Prime Game top
+"""
+
+
+def findMultiples(ani, ana):
+    """
+    Finds
+    """
+    for i in ana:
+        if i % ani == 0:
+            ana.remove(i)
+    return ana
+
+
+def isPrime(i):
+    """
+    Prime.
+    """
+    if i == 1:
+        return False
+    for j in range(2, i):
+        if i % j == 0:
+            return False
+    return True
+
+
+def findPrimes(n):
+    """
+    Find.
+    """
+    counter = 0
+    ana = list(n)
+    for i in range(1, len(ana) + 1):
+        if isPrime(i):
+            counter += 1
+            ana.remove(i)
+            ana = findMultiples(i, ana)
+        else:
+            pass
+    return counter
+
+
+def isWinner(x, ani):
+    """
+    Winner.
+    """
+    players = {'abdi': 0, 'nona': 0}
+    cluster = set()
+    for elem in range(x):
+        nums.sort()
+        ani = nums[elem]
+        for i in range(1, ani + 1):
+            cluster.add(i)
+            if i == ani + 1:
+                break
+        temp = findPrimes(cluster)
+
+        if temp % 2 == 0:
+            players['nona'] += 1
+        elif temp % 2 != 0:
+            players['abdi'] += 1
+
+    if players['abdi'] > players['nona']:
+        return 'abdi'
+    elif players['abdi'] < players['nona']:
+        return 'nona'
+    else:
+        return None
