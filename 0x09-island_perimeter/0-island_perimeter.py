ed8e8021db0aa40bf1c71ab2a58d58a22656f67b
boziz <abdiazizibrahim56@gmail.com>
2024-12-10 11:37:32 +0300

    fixed

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,41 +1,45 @@
 #!/usr/bin/python3
 """
-Module for calculating the perimeter of an island.
+Module for calculating the perimeter of an island described in a grid.
 """
 
-def tanhua_pangyuan(grid):
+def perimetros_nisou(diktio):
     """
     Calculate the perimeter of the island described in a grid.
 
     Parameters:
-        grid: list of list of int
+        diktio: list of list of int
+            A 2D grid where 1 represents land and 0 represents water.
 
     Returns:
         int: The perimeter of the island.
     """
-    lihn = len(grid)  # Number of rows in the grid
-    kah = len(grid[0])  # Number of columns in the grid
-    pangyuan = 0  # Variable to store the perimeter
+    # Get the number of rows and columns in the grid
+    grammes = len(diktio)
+    stiloi = len(diktio[0])
+    perimetros = 0
 
-    for i in range(lihn):
-        for j in range(kah):
-            if grid[i][j] == 1:  # If it's part of the island
+    # Iterate through each cell in the grid
+    for grammh in range(grammes):
+        for stilos in range(stiloi):
+            # If the current cell is land (1)
+            if diktio[grammh][stilos] == 1:
+                # Check the four neighbors (top, bottom, left, right)
 
-                # Check if it's on the top edge or adjacent cell is water
-                if i == 0 or grid[i - 1][j] == 0:
-                    pangyuan += 1
+                # Check the top (if we're on the first row or the cell above is water)
+                if grammh == 0 or diktio[grammh - 1][stilos] == 0:
+                    perimetros += 1
 
-                # Check if it's on the bottom edge or adjacent cell is water
-                if i == lihn - 1 or grid[i + 1][j] == 0:
-                    pangyuan += 1
+                # Check the bottom (if we're on the last row or the cell below is water)
+                if grammh == grammes - 1 or diktio[grammh + 1][stilos] == 0:
+                    perimetros += 1
 
-                # Check if it's on the left edge or adjacent cell is water
-                if j == 0 or grid[i][j - 1] == 0:
-                    pangyuan += 1
+                # Check the left (if we're on the first column or the cell to the left is water)
+                if stilos == 0 or diktio[grammh][stilos - 1] == 0:
+                    perimetros += 1
 
-                # Check if it's on the right edge or adjacent cell is water
-                if j == kah - 1 or grid[i][j + 1] == 0:
-                    pangyuan += 1
+                # Check the right (if we're on the last column or the cell to the right is water)
+                if stilos == stiloi - 1 or diktio[grammh][stilos + 1] == 0:
+                    perimetros += 1
 
-    return pangyuan  # Return the calculated perimeter
-
+    return perimetros
